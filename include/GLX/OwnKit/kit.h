8ad3c481633d566bf45daa93ba8a3d089ea983ca
godotc <disyourself@qq.com>
2023-02-13 21:04:45 +0800

    setup project, cannot display/draw circle

---
+++ include/GLX/OwnKit/kit.h
@@ -0,0 +1,10 @@
+
+#include <glm/vec3.hpp>
+
+class ownkit
+{
+  public:
+    static constexpr float PI = 3.1415926;
+
+    static void DrawCircle(glm::vec3 &&rgb, int steps, int radius);
+}; // namespace ownkit
---
+++ src/main.cc
@@ -0,0 +1,71 @@
+#include <gl/glew.h>
+
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <locale>
+#include <stdexcept>
+
+#include <string>
+
+#include <GLX/OwnKit/kit.h>
+
+const int WIN_WIDTH = 800;
+const int WIN_HEIGHT = 600;
+
+int main(int argc, char **argv)
+{
+    using std::cout, std::endl;
+
+    if (GL_TRUE != glfwInit())
+    {
+        throw std::runtime_error("Failed to init glfw");
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *p_Window;
+    p_Window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "GLX", nullptr, nullptr);
+    if (!p_Window)
+    {
+        glfwTerminate();
+        throw std::runtime_error("Failed to create window");
+    }
+
+    glfwMakeContextCurrent(p_Window);
+
+    // GLEW
+    if (glewInit() != GLEW_OK)
+    {
+        cout << "GLEW error !!" << endl;
+    }
+    cout << glGetString(GL_VERSION) << endl;
+
+    // Config
+    {
+        glfwSwapInterval(1);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    }
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glScalef(0.1, 0.1, 0.1);
+
+    while (!glfwWindowShouldClose(p_Window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.2, 0.3, 0.3, 1);
+
+        ownkit::DrawCircle(glm::vec3({255, 255, 255}), 20, 1);
+
+        glfwSwapBuffers(p_Window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
