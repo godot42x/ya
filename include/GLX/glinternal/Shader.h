afefe2e5d566928240b3ebd3dc595b12bda10249
godotc <disyourself@qq.com>
2023-02-20 10:27:13 +0800

    Update format to 16

--- include/GLX/glinternal/Shader.h
+++ include/GLX/glinternal/Shader.h
@@ -14,23 +14,24 @@
 #include <string>
 #include <strstream>
 
-namespace glinternal
-{
+namespace glinternal {
 
 using std::string;
 
 class Shader
 {
   public:
+    // From 2 file paths
     Shader(string &vertexShaderPath, string &fragmentShaderPath);
-    // 当 vert 与 frag 写在同一个文件
+    // When vert and frag in same files
     Shader(string &theIntengrateFile);
+    // Form 2 strings
     Shader(const char *vertexStr, const char *fragmentStr);
 
-    Shader(const Shader &) = delete;
+    Shader(const Shader &)            = delete;
     Shader &operator=(const Shader &) = delete;
 
-    Shader(Shader &&other) noexcept = default;
+    Shader(Shader &&other) noexcept            = default;
     Shader &operator=(Shader &&other) noexcept = default;
 
     ~Shader();
@@ -40,9 +41,11 @@ class Shader
     void UnUse() const;
 
     // uniform 工具函数
-    template <typename T> void SetUnifrom1(const string &name, T value) const;
+    template <typename T>
+    void SetUnifrom1(const string &name, T value) const;
 
-    [[nodiscard("Shader program ID")]] constexpr GLuint getID() const noexcept
+    [[nodiscard("Shader program ID")]]
+    constexpr GLuint getID() const noexcept
     {
         return ID;
     };
@@ -51,25 +54,24 @@ class Shader
     GLuint ID;
 
   private:
-    void initProgram(string &vertSource, string &fragSource);
-    void initProgram(const char *vertSource, const char *fragSource);
+    void          initProgram(string &vertSource, string &fragSource);
+    void          initProgram(const char *vertSource, const char *fragSource);
     static GLuint getProgram(GLuint vert, GLuint frag);
-    static void testCompile(GLuint shaderId, std::string &errorPrefix);
-    static GLuint getShader(const char *source, GLenum shaderType, std::string &&errorPrefix, GLint *place_holder);
+    static void   testCompile(GLuint shaderId, std::string &errorPrefix);
+    static GLuint getShader(const char *source, GLenum shaderType,
+                            std::string &&errorPrefix, GLint *place_holder);
 };
 
-template <typename T> void Shader::SetUnifrom1(const string &name, T value) const
+template <typename T>
+void Shader::SetUnifrom1(const string &name, T value) const
 {
-    if (std::is_same<bool, T>::value)
-    {
+    if (std::is_same<bool, T>::value) {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
     }
-    else if (std::is_same<int, T>::value)
-    {
+    else if (std::is_same<int, T>::value) {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
     }
-    else if (std::is_same<float, T>::value)
-    {
+    else if (std::is_same<float, T>::value) {
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
     }
 }
