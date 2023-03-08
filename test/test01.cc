bdce548465108199ae246c107007a3e34d2232b8
godotc <disyourself@qq.com>
2023-03-09 02:21:32 +0800

    update

--- test/test01.cc
+++ test/test01.cc
@@ -1,20 +1,28 @@
-#include "glinternal/Core.h"
-#include "logx/spdx.h"
+
+#include "spdlog/spdlog.h"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <functional>
+#include <glinternal/core.h>
 #include <gtest/gtest.h>
+#include <logx/spdx.h>
+#include <unistd.h>
 
 
 
-TEST(TestInputMapping, test01)
+TEST(TestInputMapping, InsertAndCall)
 {
+    spdlog::debug("Start inputMapping insert...");
     // TODO: test input mapping
     glinternal::Gloria ctx;
 
-    auto b = ctx.AddInputMapping(
-        GLFW_KEY_0, std::function([]() { LERROR("hello world"); return true; }));
+    auto b = ctx.AddInputMapping(GLFW_KEY_0, glinternal::MappingFn([]() { LERROR("hello world"); return true; }));
+
     EXPECT_EQ(b, true);
 
-    ctx.InputCallback(GLFW_KEY_0);
+    auto ret = ctx.InputCallback(GLFW_KEY_0);
+    spdlog::debug(ret);
+
+
+    sleep(1000);
 }
