614fa2c836af7641e0c23ef9698f90020ceab62f
godot42 <disyourself@qq.com>
2024-09-25 02:03:04 +0800

    todo: handle lifetime, why the surface have lifetime?

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,102 @@ use winit::{
     window::{Window, WindowAttributes},
 };
 
-
-struct State<'a>{
-    surface : wgpu::Surface<'a>,
-    device : wgpu::Device,
-    queue : wgpu::Queue,
-    config : wgpu::SurfaceConfiguration,
-    size : winit::dpi::PhysicalSize<u32>,
+struct State<'window> {
+    surface: wgpu::Surface<'window>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
 }
 
+impl<'window> State<'window> {
+    pub async fn new(window: &'window Window) -> Self {
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::PRIMARY,
+            ..Default::default()
+        });
+
+        let surface = instance.create_surface(window).unwrap();
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await
+            .unwrap();
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    required_features: wgpu::Features::default(),
+                    required_limits: wgpu::Limits::default(), // TODO: limit  for web
+                    label: None,
+                    memory_hints: Default::default(),
+                },
+                None, // Trace path
+            )
+            .await
+            .unwrap(); // TODO: handle error
+
+        let surface_capbilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capbilities
+            .formats
+            .iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_capbilities.formats[0]);
+        let size = window.inner_size();
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_capbilities.present_modes[0],
+            alpha_mode: surface_capbilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+        }
+    }
+
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.config.width = new_size.width;
+            self.config.height = new_size.height;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    async fn run(&mut self) {}
+}
 
 #[derive(Default)]
-struct App {
+struct App<'window> {
     window: Option<Window>,
+    state: Option<State<'window>>,
 }
 
-impl ApplicationHandler for App {
+impl ApplicationHandler for App<'_> {
     fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
         self.window = Some(
             event_loop
                 .create_window(WindowAttributes::default())
                 .unwrap(),
         );
+
+        self.state = Some(pollster::block_on(State::new(
+            self.window.as_ref().unwrap(),
+        )));
     }
 
     fn window_event(
@@ -52,7 +126,7 @@ impl ApplicationHandler for App {
                 self.window.as_ref().unwrap().request_redraw();
             }
             WindowEvent::KeyboardInput {
-                device_id,
+                device_id: DeviceId,
                 event,
                 is_synthetic,
             } => match event.logical_key {
@@ -67,9 +141,7 @@ impl ApplicationHandler for App {
     }
 }
 
-fn run() {
-    wgpu::core::instance::
-
+pub fn run() {
     let event_loop = winit::event_loop::EventLoop::new().unwrap();
     event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll); // for game loop poll, not wait in other app
 
@@ -79,5 +151,5 @@ fn run() {
 
 fn main() {
     println!("Hello, world!");
-    run();
+    run()
 }
