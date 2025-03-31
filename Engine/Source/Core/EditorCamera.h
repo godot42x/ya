9a805b9c49aeaa761c3f5291a771b29b5b90cc69
godot42 <disyourself@qq.com>
2025-04-01 00:25:18 +0800

    [editor] prepare for flying in 3d world.

--- Engine/Source/Core/EditorCamera.h
+++ Engine/Source/Core/EditorCamera.h
@@ -1,13 +1,29 @@
 #pragma once
 
-#include "glm/ext/matrix_clip_space.hpp"
-#include "glm/ext/matrix_transform.hpp"
-#include "glm/gtc/quaternion.hpp"
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 
-class EditorPerspectiveCamera
+#include "Log.h"
+
+
+struct Camera
 {
+    glm::mat4 projectionMatrix;
+    glm::mat4 viewMatrix;
+    glm::mat4 viewProjectionMatrix;
+
+    glm::mat4 getViewProjectionMatrix() const { return viewProjectionMatrix; }
+
+  protected:
+    Camera() = default;
+};
+
+class EditorCamera : public Camera
+{
+
     float fov         = 45.0f;
     float aspectRatio = 1.6 / 0.9f;
     float nearClip    = 0.1f;
@@ -16,18 +32,33 @@ class EditorPerspectiveCamera
     glm::vec3 position = {0.0f, 0.0f, 0.0f};
     glm::vec3 rotation = {0.0f, 0.0f, 0.0f}; // pitch, yaw, roll
 
-    glm::mat4 viewMatrix;
-    glm::mat4 projectionMatrix;
-    glm::mat4 viewProjectionMatrix;
+    enum EProjectionType
+    {
+        Perspective,
+        Orthographic
+    } projectionType;
+
 
   public:
-    EditorPerspectiveCamera(float fov, float aspectRatio, float nearClip, float farClip)
+    EditorCamera()
     {
-        updateProjection(fov, aspectRatio, nearClip, farClip);
-        setPosition(position);
-        setRotation(rotation);
     }
-    ~EditorPerspectiveCamera();
+    ~EditorCamera();
+
+    void setProjection(float fov, float aspectRatio, float nearClip, float farClip)
+    {
+        this->projectionType = EProjectionType::Perspective;
+        this->fov            = fov;
+        if (fov < 1.f) {
+            NE_WARN("FOV is too small {}", fov);
+        }
+        this->aspectRatio = aspectRatio;
+        this->nearClip    = nearClip;
+        this->farClip     = farClip;
+        projectionMatrix  = glm::perspective(glm::radians(fov), aspectRatio, nearClip, farClip);
+
+        recalculateViewProjectionMatrix();
+    }
 
     void recalculateViewProjectionMatrix()
     {
@@ -46,14 +77,5 @@ class EditorPerspectiveCamera
     {
         this->rotation = rotation;
         recalculateViewProjectionMatrix();
-    }
-    void updateProjection(float fov, float aspectRatio, float nearClip, float farClip)
-    {
-        this->fov            = fov;
-        this->aspectRatio    = aspectRatio;
-        this->nearClip       = nearClip;
-        this->farClip        = farClip;
-        projectionMatrix     = glm::perspective(glm::radians(fov), aspectRatio, nearClip, farClip);
-        viewProjectionMatrix = projectionMatrix * viewMatrix;
     }
 };
